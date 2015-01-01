f5cb7681a20f3cd49935afb53dec68d491692a8c
Michael Köppl <mko.mko31@gmail.com>
2015-01-01 18:22:08 +0100

    Output improvement to images.js

--- routes/images.js
+++ routes/images.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET ajax. */
 router.get('/:file', function(req, res) {
 
-    console.log('sent ' + req.params.file);
+    console.log('sent /images' + req.params.file);
 
     res.sendfile('/images' + req.params.file);
 
