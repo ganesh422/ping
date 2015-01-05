ae0c17524150845817d04be7c41e2ab356a02f54
Michael Köppl <mko.mko31@gmail.com>
2015-01-05 18:50:51 +0100

    Enable people.js to use MySQL DB
    people.js can now use a MySQL to log users in or register new users.

--- routes/people.js
+++ routes/people.js
@@ -3,40 +3,97 @@
  */
 var express = require('express');
 var router = express.Router();
-var passport = require('passport');
+var dbpool = require("../src/db");
 
 // handle registration post request here
 // request goes to /people/new from client
 router.post('/new', function(req, res){
-    // return successful json
-    res.json({ 'status': 'success' });
-
     console.log('\nPOST request for /people/new  ::--> got data');
     try{
-    	console.log(req.body.vale + '\n' + req.body.valn + '\n' + req.body.valps + '\n' + req.body.valp + '\n');
+        dbpool.getConnection(function(err, connection) {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                res.status(403).json({errorHappened:false});
+                return;
+            }
+
+            console.log('connected as id ' + connection.threadId);
+
+            var post;
+
+            if(req.body.valps == undefined){
+                post = {eaddress:req.body.vale, name:req.body.valn, wp:req.body.valp};
+            }else{
+                post = {eaddress:req.body.vale, name:req.body.valn, pseudo:req.body.valps, wp:req.body.valp};
+            }
+            
+            var query = connection.query("INSERT INTO User SET ?", post, function(err, result) {
+                if (err){
+                    console.log("ERROR AT MYSQL QUERY");
+                    console.log(err);
+                    res.status(403).json({errorHappened:false});
+                    return;
+                }
+                
+                console.log(query.sql);
+                console.log(result); 
+                res.status(200).json({isRegistered:true});
+
+                // release connection for next request
+                connection.release();
+            });
+        });
 	}catch(ex){
 		console.log('ERROR: ' + ex);
 	}
-
-    // to do
-    // work with passport to authenticate
-    //
 });
 
 router.post('/login', function(req, res){
-	res.json({'status': 'success'});
-
 	console.log('\nPOST request for /people/login  ::--> got data');
 	try{
-    	console.log(req.body.vale + '\n' + req.body.valp + '\n');
+    	dbpool.getConnection(function(err, connection) {
+            if (err) {
+                console.error('error connecting: ' + err.stack);
+                res.status(403).json({errorHappened:true});
+                return;
+            }
+
+            console.log('connected as id ' + connection.threadId);
+
+            var query = connection.query("SELECT wp from User WHERE eaddress=?", [req.body.vale], function(err, rows, fields) {
+                if (err){
+                    console.log("ERROR AT MYSQL QUERY");
+                    console.log(err);
+                    res.status(403).json({errorHappened:true});
+                    return;
+                }
+
+                if(rows[0].wp == req.body.valp){
+                    console.log("password is correct");
+                    res.status(200).json({isValid:true});
+                }else{
+                    console.log("password is invalid");
+                    res.status(403).json({isValid:false});
+                }
+
+                console.log(query.sql);
+                console.log(rows[0]);
+                console.log(req.body.valp);
+                console.log(rows[0].wp); 
+
+                // release connection for next request
+                connection.release();
+            });
+        });
 	}catch(ex){
 		console.log('ERROR: ' + ex);
 	}
 });
 
+
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('people', {title: 'Qi People'});
+    res.render('index.jade', {title: 'Qi People'});
 });
 
 module.exports = router;
