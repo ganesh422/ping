5aed38d17d85666bf59e701899baa2c884916b02
Michael Köppl <mko.mko31@gmail.com>
2015-01-25 00:38:56 +0100

    Added winston logging import and changed logging

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var logger = require('../utils/logger');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  //res.render('index', { title: 'Qi' });
-  console.log('GET request for /  ::--> returned index.jade');
-  res.render('index.jade', {"title": "Qi"});
+  logger.info('GET request for /  ::--> returned index.jade');
+  res.render('index.jade', {"title": "ping"});
 });
 
 module.exports = router;
