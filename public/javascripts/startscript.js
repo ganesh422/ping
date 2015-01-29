ffe4ff540d0f35adede920c03a6ad4ef3c3c105a
Michael Köppl <mko.mko31@gmail.com>
2015-01-29 15:09:51 +0100

    Add username storing
    The last logged in user’s ID gets stored in the session (until the tab
    gets closed) or permanently in the local storage of the browser. When
    the page is loaded, the website sends a request to the server asking
    for the username and automatically fills the login field with it.

--- public/javascripts/startscript.js
+++ public/javascripts/startscript.js
@@ -1,8 +1,18 @@
 function start(){
-  randomizeBackground();
-  animateBackground();
-  set_welcome_click_listeners();
-  set_welcome_keypress_listeners();
-  $("#registerpanel").hide();
-  $("#emailpseudoinput_login").focus();
+	randomizeBackground();
+	animateBackground();
+	set_welcome_click_listeners();
+	set_welcome_keypress_listeners();
+	$("#registerpanel").hide();
+	$("#emailpseudoinput_login").focus();
+
+  	// if there is a uid stored in the browser
+  	// get it, send it to the server and fetch the user pseudonym
+	if(window.sessionStorage.getItem("ping_uid") != undefined){
+		pseudolookup_insert_uname(window.sessionStorage.getItem("ping_uid"));
+	}else if(window.localStorage.getItem("ping_uid") != undefined){
+		pseudolookup_insert_uname(window.localStorage.getItem("ping_uid"));
+	}else{
+		console.log("no uid and no username found.");
+	}
 }
