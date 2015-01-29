4b2e08706ab5442536485859d0dacf88aca3c1f7
Michael Köppl <mko.mko31@gmail.com>
2015-01-29 15:10:38 +0100

    Update all the schemas

--- utils/dbschemes/post.js
+++ utils/dbschemes/post.js
@@ -2,12 +2,16 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = ObjectId = Schema.ObjectId;
 var crypto = require('crypto');
+var User = require('User').User;
 
 /* POST */
 var postSchema = mongoose.Schema({
 	text: { type: String, required: true },
 	imgsrc: { type: String },
+	creators[{ type: ObjectId, ref: 'User' }]
     comments: [
+    	creator: { type: ObjectId, ref: 'User' },
+    	text: String
     ]
 });
 
--- utils/dbschemes/sub.js
+++ utils/dbschemes/sub.js
@@ -2,11 +2,14 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = ObjectId = Schema.ObjectId;
 var crypto = require('crypto');
+var Post = require('post.js').Post;
+var User = require('user.js').User;
 
 /* COMMUNITY */
 var subSchema = Schema({
 	name: { type: String, required: true, unique: true },
 	date_creation: { type: Date, default: Date.now },
+	posts [{ type: ObjectId, ref: 'Post' }]
 });
 
 module.exports.Sub = mongoose.model('Sub', subSchema);
