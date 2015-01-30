ebaaad4ab506b2f7cacab32b19dd314616c01645
Michael Köppl <mko.mko31@gmail.com>
2015-01-30 12:49:30 +0100

    Moved welcome start script to welcome.js

--- public/javascripts/startscript.js
+++ public/javascripts/startscript.js
@@ -1,18 +1,8 @@
-function start(){
-	randomizeBackground();
-	animateBackground();
-	set_welcome_click_listeners();
-	set_welcome_keypress_listeners();
-	$("#registerpanel").hide();
-	$("#emailpseudoinput_login").focus();
+function start_welcome(){
+	// function in welcome.js
+	welcome_start();
+}
 
-  	// if there is a uid stored in the browser
-  	// get it, send it to the server and fetch the user pseudonym
-	if(window.sessionStorage.getItem("ping_uid") != undefined){
-		pseudolookup_insert_uname(window.sessionStorage.getItem("ping_uid"));
-	}else if(window.localStorage.getItem("ping_uid") != undefined){
-		pseudolookup_insert_uname(window.localStorage.getItem("ping_uid"));
-	}else{
-		console.log("no uid and no username found.");
-	}
+function start_home(){
+
 }
