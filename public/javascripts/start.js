2c1c9fbbf4b86f50ee9abede366054eb133718fe
Michael Köppl <mko.mko31@gmail.com>
2015-01-31 19:06:20 +0100

    Rename and update start script
    - the script is renamed to start.js
    - it gets a redirect function (if user calls http, he will be
    redirected in the future. for now he’s being alerted)

---
+++ public/javascripts/start.js
@@ -0,0 +1,22 @@
+function https_redirect(){
+	// only works with http and https standard ports
+	// redirects to https page
+	/*if(window.location.protocol == "http:"){
+		restOfUrl = window.location.href.substr(5);
+		window.location.replace("https:" + restOfUrl);
+	}*/
+
+	// because it only works with standard ports, we use this for now
+	if(window.location.protocol == "http:"){
+		$("#errors").text("HTTP is very insecure, please visit the HTTPS page.");
+	}
+}
+
+function start_welcome(){
+	// function in welcome.js
+	welcome_start();
+}
+
+function start_home(){
+
+}
