ff0cb84781e18ac0919014295549cbef8ee1b25e
Michael Köppl <mko.mko31@gmail.com>
2015-02-18 22:57:03 +0100

    Added HTTP about message and updated INVALID_WP

--- utils/statics.js
+++ utils/statics.js
@@ -13,7 +13,7 @@ var EMAIL_IN_USE = 'This e-mail address is in use.';
 var PSEUDO_IN_USE = 'This pseudonym is in use.';
 var REGISTRATION_SUC = 101;
 var LOGIN_SUC = 102;
-var INVALID_WP = 'The password you entered is invalid!';
+var INVALID_WP = 'The password you entered is invalid.';
 var ACCOUNT_NOT_FOUND = 'There is no account with that e-mail/pseudonym.';
 
 module.exports.INTERNAL_ERROR = INTERNAL_ERROR;
@@ -33,3 +33,6 @@ var SALT_LENGTH = 32;
 module.exports.PBKDF2_ITERATIONS = ITERATIONS;
 module.exports.PBKDF2_LENGTH = LENGTH;
 module.exports.SALT_LENGTH = SALT_LENGTH;
+
+var ABOUT_HTTP_MESSAGE = "Hypertext Transfer Protocol (HTTP) is the way servers and browsers talk to each other. It’s a great language for computers, but it’s not encrypted. Think of it this way. If everyone in the world spoke English, everyone would understand each other. Every browser and server in the world speaks HTTP, so if an attacker managed to hack in, he could read everything going on in the browser, including that username and password you just typed in. Don't get us wrong. Nothing is ever 100% secure, but using HTTPS lowers the risk of someone reading everything you do and all your personal data."
+module.exports.ABOUT_HTTP_MESSAGE = ABOUT_HTTP_MESSAGE;
