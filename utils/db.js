43c19effb0bf723c77e452a94425f038fa5605c6
Michael Köppl <mko.mko31@gmail.com>
2015-04-07 12:16:46 +0200

    Added new post gathering algorithm, changed all community names to be lower case and updated some methods
    The new post gathering algorithm combines posts by the user’s
    communities and his followed friends and sorts them by date.

--- utils/db.js
+++ utils/db.js
@@ -12,6 +12,7 @@ var statics = require('../utils/statics');
 var misc = require('../utils/misc');
 var crypto = require('crypto');
 var config = require('../config');
+var async = require('async');
 
 // store all the user's usernames
 var userlist =[];
@@ -33,14 +34,16 @@ module.exports = {
 		find_posts_by_sub(subname, ip, callback);
 	}, find_posts_by_sublist: function(pseudonym, ip, callback){ // find posts by the user's list of communities
 		find_posts_by_sublist(pseudonym, ip, callback);
-	}, find_posts_by_friendlist: function(pseudonym, ip, callback){
-		find_posts_by_friendlist(pseudonym, ip, callback);
+	}, find_posts_by_friend: function(pseudonym, ip, callback){
+		find_posts_by_friend(pseudonym, ip, callback);
 	}, find_posts_by_pseudonym: function(pseudonym, ip, callback){ // find posts for a user (communities, followed people)
 		find_posts_by_pseudonym(pseudonym, ip, callback)
 	}, follow: function(senderpseudonym, whouserwantstofollow, ip, callback){
 		follow(senderpseudonym, whouserwantstofollow, ip, callback);
-	}, insert_new_sub: function(name, admin, ip, callback){ // create a new community
+	}, insert_new_sub: function(name, admin, ip, callback) { // create a new community
 		insert_new_sub(name, admin, ip, callback);
+	}, add_sub_to_pseudonym: function(pseudonym, subname, ip, callback){
+		add_sub_to_pseudonym(pseudonym, subname, ip, callback);
 	}, fetch_subs: function(pseudonym, ip, callback){ // get the list of communities the user followed
 		fetch_subs(pseudonym, ip, callback);
 	}, insert_new_post: function(creator, title, text, sub, ip, callback){ // create a new post
@@ -122,10 +125,12 @@ function login_mdb(emailpseudonym, passwd, ip, returnData /*callback*/){
         returnData(statics.INTERNAL_ERROR);
 	});
 
+	var query;
+
 	if(misc.checkIfEmailInString(emailpseudonym)){
-		var query = User.findOne({'email': emailpseudonym.toLowerCase()});
+		query = User.findOne({'email': emailpseudonym.toLowerCase()});
 	}else{
-		var query = User.findOne({'pseudonym': emailpseudonym});
+		query = User.findOne({'pseudonym': emailpseudonym});
 	}
 
 	query.select('passwd salt pseudonym _id email');
@@ -196,117 +201,6 @@ function find_user_by_pseudonym(pseudonym, ip, returnData){
 	});
 }
 
-/*
- * get list of posts posted by the user
- */
-function find_posts_by_creator_pseudonym(pseudonym, ip, returnData){
-	dbcon.on('error', function(err){
-		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        returnData(statics.INTERNAL_ERROR);
-	});
-
-	Post.find({'creators':pseudonym}).lean().exec(function(err, result){ //lean() returns the data as a JS object instead of mongoose object
-		if(err){
-			logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        	returnData(statics.INTERNAL_ERROR);
-		}
-
-		if(result){
-			returnData(null, result.reverse()); // reverse to turn the whole array around
-		}else{
-			logger.warn(ip.white.bold + ': query for posts by user (credentials: ' + pseudonym.white + ') returned no results.');
-			returnData(statics.NO_POSTS_FOUND);
-		}
-	});
-}
-
-function find_posts_by_sub(subname, ip, returnData){
-	dbcon.on('error', function(err){
-		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        returnData(statics.INTERNAL_ERROR);
-	});
-
-	Post.find({'sub': subname}).lean().exec(function(err, result){
-		if(err){
-			logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        	returnData(statics.INTERNAL_ERROR);
-		}
-
-		if(result){
-			returnData(result.reverse());
-		}
-	});
-}
-
-/*
- * get list of posts contained in subs the user subscribed to
- */
-function find_posts_by_sublist(pseudonym, ip, returnData){
-	dbcon.on('error', function(err){
-		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        returnData(statics.INTERNAL_ERROR);
-	});
-
-	fetch_subs(pseudonym, ip, function(response){
-		if(response){
-			Post.find({'sub': {$in: response}}).lean().exec(function(err, result){
-				if(err){
-					logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        			returnData(statics.INTERNAL_ERROR);
-				}
-
-				if(result){
-					returnData(result.reverse());
-				}
-			});
-		}
-	});
-}
-
-/*function find_posts_by_friendlist(pseudonym, ip, returnData){
-	dbcon.on('error', function(err){
-		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        returnData(statics.INTERNAL_ERROR);
-	});
-
-	User.findOne({'pseudonym': pseudonym}).select('friends').exec(function(err, result){
-		if(err){
-			logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        	returnData(statics.INTERNAL_ERROR);
-		}
-
-		if(result){
-			logger.debug(result.friends);
-
-			var postlist_followed = [];
-
-			for(i in result){
-				find_posts_by_creator_pseudonym(result[i], ip, function(status, response){
-					for(j in response){
-						postlist_followed.push(reponse[j]);
-					}
-				});
-			}
-
-			console.log(postlist_followed);
-		}else{
-		}
-	});
-}
-
-function find_posts_by_pseudonym(pseudonym, ip, returnData){
-	dbcon.on('error', function(err){
-		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        returnData(statics.INTERNAL_ERROR);
-	});
-
-	find_posts_by_sublist(pseudonym, ip, function(response){
-		if(response){
-
-		}
-	});
-}*/
-
 function follow(senderpseudonym, whouserwantstofollow, ip, returnData){
 	dbcon.on('error', function(err){
 		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
@@ -335,7 +229,7 @@ function insert_new_sub(name, admin, ip, returnData){
 	// the new user itself is created based on the model
 	// the data given by the user is inserted
 	var newSub = new Sub({
-		name: name,
+		name: name.toLowerCase(),
 	    admins: admin
 	});
 
@@ -352,7 +246,7 @@ function insert_new_sub(name, admin, ip, returnData){
             // successful
 
             // add newly created sub to the creator's subs
-			User.findAndModify({'pseudonym': admin}, [], {$addToSet: {'subs': name}}, {}, function(err){
+			User.findAndModify({'pseudonym': admin}, [], {$addToSet: {'subs': name.toLowerCase()}}, {}, function(err){
 					if(err){
 						logger.error(err.toString());
 					}else{
@@ -365,29 +259,42 @@ function insert_new_sub(name, admin, ip, returnData){
 	});
 }
 
+function add_sub_to_pseudonym(pseudonym, subname, ip, returnData){
+	dbcon.on('error', function(err){
+		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+		returnData(result_status = statics.INTERNAL_ERROR);
+	});
+
+	User.findAndModify({'pseudonym': pseudonym}, [], {$addToSet: {'subs': subname.toLowerCase()}}, {}, function(err){
+			if(err){
+				logger.error(err.toString());
+			}else{
+				logger.info(ip.white.bold + ': ' + pseudonym.white.bold + ' joined ' + subname.white.bold + '!');
+				returnData('You joined ' + subname + '.');
+			}
+		}
+	);
+}
+
 function fetch_subs(pseudonym, ip, returnData){
 	dbcon.on('error', function(err){
 		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
         returnData(result_status = statics.INTERNAL_ERROR);
 	});
 
-	var query = User.findOne({'pseudonym': pseudonym});
-
-    query.select('subs');
-
-    query.exec(function(err, result){
-    	if(err){
+	User.findOne({'pseudonym': pseudonym}).select('subs').lean().exec(function(err, result){
+		if(err){
 			logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
-        	returnData(statics.INTERNAL_ERROR);
+			returnData(statics.INTERNAL_ERROR);
 		}
 
 		if(result){
-		    returnData(result.subs)
+			returnData(result.subs)
 		}else{
-		    logger.warn(ip.white.bold + ': query for user (credentials: ' + pseudonym + ') returned no result.');
+			logger.warn(ip.white.bold + ': query for user (credentials: ' + pseudonym + ') returned no result.');
 			returnData(statics.ACCOUNT_NOT_FOUND);
 		}
-    });
+	});
 }
 
 // =============================================
@@ -405,7 +312,7 @@ function insert_new_post(creator, title, text, sub, ip, returnData){
 		title: title,
 		text: text,
 	    creators: creator,
-	    sub: sub
+	    sub: sub.toLowerCase()
 	});
 
 	newPost.save(function(err){
@@ -416,5 +323,156 @@ function insert_new_post(creator, title, text, sub, ip, returnData){
             logger.info(ip.white.bold + ': new post "' + title.white.bold + '" from user ' + creator.white.bold + ' created in ' + sub.white.bold + '!');
             returnData(statics.NEWPOST_SUC);
         }
+	});
+}
+
+/*
+ * get list of posts posted by the user
+ */
+function find_posts_by_creator_pseudonym(pseudonym, ip, returnData){
+	dbcon.on('error', function(err){
+		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+		returnData(statics.INTERNAL_ERROR);
+	});
+
+	Post.find({'creators':pseudonym}).sort({'date_created': 'desc'}).limit(20).lean().exec(function(err, result){ //lean() returns the data as a JS object instead of mongoose object
+		if(err){
+			logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+			returnData(statics.INTERNAL_ERROR);
+		}
+
+		if(result){
+			returnData(null, result); // reverse to turn the whole array around
+		}else{
+			logger.warn(ip.white.bold + ': query for posts by user (credentials: ' + pseudonym.white + ') returned no results.');
+			returnData(statics.NO_POSTS_FOUND);
+		}
+	});
+}
+
+function find_posts_by_sub(subname, ip, returnData){
+	dbcon.on('error', function(err){
+		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+		returnData(statics.INTERNAL_ERROR);
+	});
+
+	Post.find({'sub': subname}).sort({'date_created': 'desc'}).limit(20).lean().exec(function(err, result){
+		if(err){
+			logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+			returnData(statics.INTERNAL_ERROR);
+		}
+
+		if(result){
+			returnData(result);
+		}else{
+			logger.warn(ip.white.bold + ': query for posts by sub ' + subname.white + ' returned no results.');
+			returnData(statics.NO_POSTS_FOUND);
+		}
+	});
+}
+
+/*
+ * get list of posts contained in subs the user subscribed to
+ */
+function find_posts_by_sublist(pseudonym, ip, returnData){
+	dbcon.on('error', function(err){
+		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+		returnData(statics.INTERNAL_ERROR);
+	});
+
+	fetch_subs(pseudonym, ip, function(response){
+		if(response){
+			Post.find({'sub': {$in: response}}).sort({'date_created': 'desc'}).limit(20).lean().exec(function(err, result){
+				if(err){
+					logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+					returnData(statics.INTERNAL_ERROR);
+				}
+
+				if(result){
+					returnData(result);
+				}else{
+					returnData(statics.NO_POSTS_FOUND);
+				}
+			});
+		}
+	});
+}
+
+/*
+ * get list of posts by the user's list of followed people
+ */
+function find_posts_by_friend(pseudonym, ip, returnData){
+	dbcon.on('error', function(err){
+		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+		returnData(statics.INTERNAL_ERROR);
+	});
+
+	User.findOne({'pseudonym': pseudonym}).select('friends').lean().exec(function(err, result){
+		if(err){
+			logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+			returnData(statics.INTERNAL_ERROR);
+		}
+
+		if(result){
+			var postlist = [];
+			var index = 0;
+
+			(function fetchLoop(){
+				if(index <= result.friends.length){
+					Post.find({'creators': result.friends[index]}).limit(5).sort({'date_created': 'desc'}).lean().exec(function(err, res){
+						if(err){
+							logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+						}
+
+						if(res){
+							res.forEach(function(entry){
+								postlist.push(entry);
+								fetchLoop();
+							});
+						}else{
+							fetchLoop();
+						}
+					});
+				}
+
+				index++;
+			}());
+			returnData(postlist);
+		}else{
+			returnData(statics.NO_POSTS_FOUND);
+		}
+	});
+}
+
+/*
+ * find the posts of the communities and the people a user (pseudonym) followed combined
+ * probably needs a rename :D
+ */
+function find_posts_by_pseudonym(pseudonym, ip, returnData){
+	dbcon.on('error', function(err){
+		logger.error(err.toString().cyan.italic + '. Is ' + ' mongod '.red.bold + ' running?');
+		returnData(statics.INTERNAL_ERROR);
+	});
+
+	find_posts_by_friend(pseudonym, ip, function(response_friendlist){
+		if(response_friendlist && response_friendlist != statics.NO_POSTS_FOUND && response_friendlist != statics.INTERNAL_ERROR){
+			find_posts_by_sublist(pseudonym, ip, function(response_subs){
+				if(response_subs && response_subs != statics.NO_POSTS_FOUND && response_subs != statics.INTERNAL_ERROR) {
+					response_friendlist.forEach(function (ent1) {
+						response_subs.forEach(function (ent2) {
+							if (ent1.id == ent2.id) {
+								response_subs.splice(response_subs.indexOf(ent2));
+							}
+						});
+					});
+					if (response_subs != undefined && response_friendlist != undefined) {
+						// combine arrays and sort by date
+						returnData(response_friendlist.concat(response_subs).sort(function (date1, date2) {
+							return date2.date_created - date1.date_created
+						}));
+					}
+				}
+			});
+		}
 	});
 }
